97e8f12b31f307abe1fc9d4d89dd69053bb8c533
Daron Yondem <daron@yondem.com>
2022-11-15 15:21:50 +0300

    Sample Commit

---
+++ SQLQuery_1.sql
@@ -0,0 +1,23 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ErrorLog2](
+	[ErrorLogID] [int] IDENTITY(1,1) NOT NULL,
+	[ErrorTime] [datetime] NOT NULL,
+	[UserName] [sysname] NOT NULL,
+	[ErrorNumber] [int] NOT NULL,
+	[ErrorSeverity] [int] NULL,
+	[ErrorState] [int] NULL,
+	[ErrorProcedure] [nvarchar](126) NULL,
+	[ErrorLine] [int] NULL,
+	[ErrorMessage] [nvarchar](4000) NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ErrorLog] ADD  CONSTRAINT [PK_ErrorLog_ErrorLogID] PRIMARY KEY CLUSTERED 
+(
+	[ErrorLogID] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ErrorLog] ADD  CONSTRAINT [DF_ErrorLog_ErrorTime]  DEFAULT (getdate()) FOR [ErrorTime]
+GO
